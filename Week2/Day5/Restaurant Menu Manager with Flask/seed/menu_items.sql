04a57f7ce23d938bc534b21a07e646f716c366e3
Hajar320 <hajarkassmi.hk@gmail.com>
2025-10-24 10:24:41 +0100

    Add files via upload

---
+++ Week2/Day5/Restaurant Menu Manager with Flask/seed/menu_items.sql
@@ -0,0 +1,14 @@
+select * from menu_items
+truncate table menu_items;
+
+insert into menu_items (item_name, item_price )
+values ('chicken tajine',20),
+       ('Beef tajine', 23),
+       ('Lamp tajine', 25),
+       ('couscous', 40),
+       ('Harira', 15),
+       ('Briouats', 10),
+       ('Chebakia', 8),
+       ('Mint tea', 5),
+         ('Moroccan salad', 12);
+
