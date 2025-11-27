e2f8645ec65971e1410e142f27ca94edfe143ec2
Hajar320 <hajarkassmi.hk@gmail.com>
2025-11-27 21:38:36 +0100

    Update database.js

--- Week6/Day3/ExercisesXP/Exercise1/blog-api/server/config/database.js
+++ Week6/Day3/ExercisesXP/Exercise1/blog-api/server/config/database.js
@@ -1,14 +1,15 @@
 const knex = require('knex');
-
+// db example
 const db = knex({
   client: 'pg',
   connection: {
     host: 'localhost',
     port: 5432,
     user: 'postgres',
-    password: '123456789',
+    password: 'enter_password_here',
     database: 'blog_db'
   },
   pool: { min: 0, max: 7 }
 });
+
 module.exports = db;
